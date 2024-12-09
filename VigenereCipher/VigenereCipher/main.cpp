e5efe4365f0dac42683b2d7112e454c21ff382ba
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-12-08 19:18:08 -0800

    Pushing final version of the project without readme complete

--- VigenereCipher/VigenereCipher/main.cpp
+++ VigenereCipher/VigenereCipher/main.cpp
@@ -1,112 +1,111 @@
 // Austin Cavanagh
 // CIS-7 Online
-// 11/06/24
+// 12/08/24
 // Course Project
 
 #include <iostream>
 #include <string>
 using namespace std;
 
+// Define constants
 const string CHAR_STR = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&* ";
+const string BUFFER = "==========================================";
 
+// Define prototypes
 string displayMenu();
-string getInput(string variableName);
-bool isValidInput(const string &input);
-
-string resizeKey(const string &key, size_t textLength);
-
-char encryptChar(char decryptedTextChar, char keyTextChar);
-char decryptChar(char encryptedTextChar, char keyTextChar);
+void handleEncryption(string &, string &, string &);
+void handleDecryption(string &, string &, string &);
+string getInput(string );
+bool isValidInput(const string &);
+string resizeKey(const string &, size_t);
+string encryptStr(string, string);
+string decryptStr(string, string);
+void displayEndMessage();
 
 int main() {
-    // Declare variables to hold the key, plain text, and encrypted text
-    string key;
-    string encryptedText;
-    string decryptedText;
-
+    // Define variables
+    string keyStr;
+    string encryptedStr;
+    string decryptedStr;
     string menuSelection;
     
     // Loop through program until used selections option 3 to exit
     do {
         // Clear variables at the start of each iteration to avoid errors
-        key.clear();
-        encryptedText.clear();
-        decryptedText.clear();
+        keyStr.clear();
+        encryptedStr.clear();
+        decryptedStr.clear();
         
         // Display menu to the user
         menuSelection = displayMenu();
         
         // Handle encrypting a message
         if (menuSelection == "1") {
-            decryptedText = getInput("Unencrypted Message");
-            key = getInput("Key");
-            key = resizeKey(key, decryptedText.length());
-            
-            for (size_t i = 0; i < decryptedText.length(); i++)
-                encryptedText += encryptChar(decryptedText[i], key[i]);
-
-            cout << "Encrypted Message: " << encryptedText << endl;
+            handleEncryption(keyStr, encryptedStr, decryptedStr);
         }
         
         // Handle decrypting a message
         if (menuSelection == "2") {
-            encryptedText = getInput("Encrypted Message");
-            key = getInput("Key");
-            key = resizeKey(key, encryptedText.length());
-            
-            for (size_t i = 0; i < encryptedText.length(); i++)
-                decryptedText += decryptChar(encryptedText[i], key[i]);
-
-            cout << "Decrypted Message: " << decryptedText << endl;
+            handleDecryption(keyStr, encryptedStr, decryptedStr);
         }
     } while (menuSelection != "3");
+    
+    // Display thank you message
+    displayEndMessage();
     
     return 0;
 }
 
-
-
-// Function resizes key to match the length of the text is is translating
-string resizeKey(const string &key, size_t textLength) {
-    // Declare variables for the new key and the old key length
-    string newKey;
-    size_t keyLength = key.size();
-    
-    // Build the new key to be the same length as the text we are translating
-    for (size_t i = 0; i < textLength; i++)
-        newKey += key[i % keyLength];
-    
-    // Return new key
-    return newKey;
+// Function handles encryption logic
+void handleEncryption(string &keyStr, string &encryptedStr, string &decryptedStr) {
+    decryptedStr = getInput("Unencrypted Message");
+    keyStr = getInput("Key");
+    keyStr = resizeKey(keyStr, decryptedStr.length());
+    encryptedStr = encryptStr(decryptedStr, keyStr);
+    cout << BUFFER << endl;
+    cout << "Encrypted Message: " << encryptedStr << endl;
 }
 
-
-
+// Function handles decryption logic
+void handleDecryption(string &keyStr, string &encryptedStr, string &decryptedStr) {
+    encryptedStr = getInput("Encrypted Message");
+    keyStr = getInput("Key");
+    keyStr = resizeKey(keyStr, encryptedStr.length());
+    decryptedStr = decryptStr(encryptedStr, keyStr);
+    cout << BUFFER << endl;
+    cout << "Decrypted Message: " << decryptedStr << endl;
+}
 
 // Function displays menu to user and collects menu selection
 string displayMenu() {
     string input;
     bool validInput = false;
     
-    cout << "Menu options" << endl;
-    cout << "1) Encrypt Message" << endl;
-    cout << "2) Decrypt Message" << endl;
-    cout << "3) Exit program" << endl;
-    
     do {
+        // Display menu options
+        cout << BUFFER << endl;
+        cout << "            ** Menu Options **            " << endl;
+        cout << "1) Encrypt Message" << endl;
+        cout << "2) Decrypt Message" << endl;
+        cout << "3) Exit program" << endl << endl;
         cout << "Select Option (1-3): ";
         getline(cin, input);
         
-        if (input == "1" || input == "2" || input == "3")
+        // Validate input and display error if invalid
+        if (input == "1" || input == "2" || input == "3") {
             validInput = true;
-        else
-            cout << "Error" << endl;
+        } else {
+            cout << BUFFER << endl;
+            cout << "     ** ERROR: Invalid menu option **     " << endl;
+        }
     } while (!validInput);
+    
+    cout << BUFFER << endl;
     
     return input;
 }
 
-// Function collects inputs for keyText, decryptedText, and encryptedText
+// Function collects inputs for keyStr, decryptedStr, and encryptedStr
 string getInput(string variableName) {
     // Declare variables for input and input validation
     string input;
@@ -117,81 +116,104 @@ string getInput(string variableName) {
         cout << variableName << ": ";
         getline(cin, input);
         
-        if (isValidInput(input))
+        // Check if input is valid
+        if (isValidInput(input)) {
             validInput = true;
-        else
-            cout << "Error" << endl;
+        } else {
+            cout << BUFFER << endl;
+            cout << "        ** ERROR: Invalid input **        " << endl;
+            cout << BUFFER << endl;
+        }
     } while (!validInput);
     
-    // Return input0
+    // Return valid input
     return input;
 }
 
-// Function validates inputs for keyText, decryptedText, and encryptedText
+// Function validates inputs for keyStr, decryptedStr, and encryptedStr
 bool isValidInput(const string &input) {
     // Return false for an empty input
-    if (input.empty())
+    if (input.empty()) {
         return false;
+    }
     
-    // Loop over input and validate each character is also in CHAR_STR
+    // Loop over input and validate that each character is in CHAR_STR
     for (char c : input) {
-        // Return false if the char is not in CHAR_STR
-        if (CHAR_STR.find(c) == string::npos)
+        // Return false if the character is not in CHAR_STR
+        // .find() will return the index if the character is found
+        // .find() will return string::npos if the character is not found
+        if (CHAR_STR.find(c) == string::npos) {
             return false;
+        }
     }
     
     // Return true if all chars are in CHAR_STR
     return true;
 }
 
-// Function accepts the decryptedText char and keyText char and returns the encrypted char
-string encryptChar(string decryptedText, string keyText) {
-    string encryptedText = "";
+// Function resizes key to match the length of the string is is translating
+string resizeKey(const string &key, size_t strLength) {
+    // Declare variables for the new key and the old key length
+    string newKey;
+    size_t keyLength = key.size();
     
-    for (int i = 0; i < decryptedText.length(); i++) {
+    // Build the new key to be the same length as the string we are translating
+    for (size_t i = 0; i < strLength; i++) {
+        // If the key is smaller than the string loop over and duplicate to match the length
+        newKey += key[i % keyLength];
+    }
+    
+    // Return new key
+    return newKey;
+}
+
+// Function accepts decryptedStr and keyStr and returns the encrypted string
+string encryptStr(string decryptedStr, string keyStr) {
+    string encryptedStr = "";
+    
+    for (int i = 0; i < decryptedStr.length(); i++) {
         // .find() searches for char in a string
         // returns index of char if found
         // returns string::npos if not found
-        size_t decryptedTextIndex = CHAR_STR.find(decryptedText[i]);
-        size_t keyTextIndex = CHAR_STR.find(keyText[i]);
+        size_t decryptedStrIndex = CHAR_STR.find(decryptedStr[i]);
+        size_t keyStrIndex = CHAR_STR.find(keyStr[i]);
         
-        // Add keyTextIndex to decryptedTextIndex to find encryptedTextIndex
+        // Add keyStrIndex to decryptedStrIndex to find encryptedStrIndex
         // Perform modulo operation to avoid overflow
-        size_t encryptedTextIndex = (decryptedTextIndex + keyTextIndex) % CHAR_STR.length();
+        size_t encryptedStrIndex = (decryptedStrIndex + keyStrIndex) % CHAR_STR.length();
         
-        //
-        encryptedText += CHAR_STR[encryptedTextIndex];
+        // Add encrypted char to encryptedStr
+        encryptedStr += CHAR_STR[encryptedStrIndex];
     }
     
-    return encryptedText;
-    
-    
-    // .find() searches for char in a string
-    // returns index of char if found
-    // returns string::npos if not found
-    size_t decryptedTextIndex = CHAR_STR.find(decryptedTextChar);
-    size_t keyTextIndex = CHAR_STR.find(keyTextChar);
-    
-    // Add keyTextIndex to decryptedTextIndex to find encryptedTextIndex
-    // Perform modulo operation to avoid overflow
-    size_t encryptedTextIndex = (decryptedTextIndex + keyTextIndex) % CHAR_STR.length();
-    
-    // Return new encrypted char
-    return CHAR_STR[encryptedTextIndex];
+    return encryptedStr;
 }
 
-// Function accepts the encryptedText char and keyText char and returns the decrypted char
-char decryptChar(char encryptedTextChar, char keyTextChar) {
-    // .find() searches for char in a string
-    // returns index of char if found
-    // returns string::npos if not found
-    size_t encryptedTextIndex = CHAR_STR.find(encryptedTextChar);
-    size_t keyTextIndex = CHAR_STR.find(keyTextChar);
+
+// Function accepts encryptedStr and keyStr and returns the decrypted string
+string decryptStr(string encryptedStr, string keyStr) {
+    string decryptedStr = "";
     
-    // Subtract keyTextIndex from encryptedTextIndex to find decryptedTextIndex
-    // Perform modulo operation to avoid overflow
-    size_t decryptedTextIndex = (encryptedTextIndex + CHAR_STR.length() - keyTextIndex) % CHAR_STR.length();
-    
-    // Return new decrypted char
-    return CHAR_STR[decryptedTextIndex];
+    for (int i = 0; i < encryptedStr.length(); i++) {
+        // .find() searches for char in a string
+        // returns index of char if found
+        // returns string::npos if not found
+        size_t encryptedStrIndex = CHAR_STR.find(encryptedStr[i]);
+        size_t keyStrIndex = CHAR_STR.find(keyStr[i]);
+        
+        // Subtract keyStrIndex from encryptedStrIndex to find decryptedStrIndex
+        // Perform modulo operation to avoid overflow
+        size_t decryptedStrIndex = (encryptedStrIndex + CHAR_STR.length() - keyStrIndex) % CHAR_STR.length();
+        
+        // Add decrypted char to decryptedStr
+        decryptedStr += CHAR_STR[decryptedStrIndex];
+    }
+
+    return decryptedStr;
+}
+
+// Function displays end message to the user
+void displayEndMessage() {
+    cout << "Thank you for using the application today!" << endl;
+    cout << BUFFER << endl;
 }
